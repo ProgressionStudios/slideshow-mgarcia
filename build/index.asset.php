a77e75594df811931d2af62ecb23ea3769cacb83
ProgressionStudios <mike@progressionstudios.com>
2024-01-07 20:47:12 -0800

    Build code for production
    npm run build to finalize code

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4ee2029ba953cbc6db19');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5b698d3417ead78fe852');
