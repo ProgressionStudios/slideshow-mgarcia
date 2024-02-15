3ffee62e8400ad7ad6b33c85458d3e3a35fb0444
Michael Garcia <mike@progressionstudios.com>
2024-02-15 08:26:48 -0800

    Update package-lock.json

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '851fb3d77d827ac3706b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '485cdfd3bb57bd6394c2');
